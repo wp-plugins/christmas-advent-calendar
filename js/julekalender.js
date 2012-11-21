c3beafbade3a7c36781de1a5c0c83365a0a341a2
stian4000 <stian4000@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-21 07:27:45 +0000

    changes made to api reflected
    git-svn-id: https://plugins.svn.wordpress.org/christmas-advent-calendar/trunk@628042 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/julekalender.js
+++ js/julekalender.js
@@ -10,16 +10,16 @@ var MakenewsmailJulekalender = (function( $ ) {
 	
 	Julekalender.prototype.generatePNG = function(url) {
 		var URL2PNG_APIKEY = "P4F33918CBA1A4",
-			URL2PNG_SECRET = "S96C086057F262",
+		    URL2PNG_SECRET = "S96C086057F262",
 			query_str = "url=" + encodeURIComponent(url) + "&fullscreen=false&thumbnail_max_width=61&format=png",
 			token = hex_md5(query_str + URL2PNG_SECRET);
 								
-			return 'http://beta.url2png.com/v6/' + URL2PNG_APIKEY + '/' + token + '/png/?' + query_str;
+			return 'http://beta.url2png.com/v6/' + URL2PNG_APIKEY + '/' + token + '/png/?' + query_str;  
 	};
 
 	Julekalender.prototype.init = function() {
 		this.getCalendars();
-	};
+	}
 	
 	Julekalender.prototype.getCalendars = function() {
 		var self = this;
@@ -30,23 +30,38 @@ var MakenewsmailJulekalender = (function( $ ) {
 			dataType: 'jsonp',
 			url: this.apiurl,
 			crossDomain: true,
-			jsonpCallback: 'tiltnes'
-		}).done(function(json) { self.writePage(json);});
-	};
+			jsonpCallback: 'tiltnes',
+		}).done(function(json) { self.writePage(json);})
+	}
 
 	Julekalender.prototype.writePage = function( json ) {
 		var local = json,
-			newarr = [],
+			newarr = [], 
 			counter = 1;
 									
 			(this.numCalendars > local.length) ? counter = local.length : counter = this.numCalendars;
 			
-			for(i=0;i<counter;i++) {
+			for(i=0;i<counter;i++) { 
 				local_language = local[i].language.substring(0, 2);
 				
 				if( this.lang === local_language ) {
+										
+					if( !local[i].path.match(/(julekalender.com)$/) ) {
+						local[i].path = local[i].path + "julekalender.com";
+					}else{
+						local[i].path = local[i].path;	
+					}
+					
+					if(local[i].title.indexOf(" ") !== -1) {
+						var title = local[i].title.split(" ");
+						local[i].title = title[0];
+					}else{
+						local[i].title = local[i].title;
+					}
+					
 					thumb = this.generatePNG(local[i].path);
 					local[i].thumbnail = thumb;
+					
 					newarr.push(local[i]);
 				}
 			}
@@ -54,13 +69,14 @@ var MakenewsmailJulekalender = (function( $ ) {
 			template = Handlebars.compile($('#template').html());
 			var temp = template(this.shuffle(newarr));
 			
-			$('ul#julekalender').append(temp);
-	};
+			$('ul#julekalender').append(temp)
+	}
 
 	Julekalender.prototype.shuffle = function(arr) {
 		for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
-		return arr;
-    };
+    	return arr;
+    	
+	}
 
 	return Julekalender;
 	
